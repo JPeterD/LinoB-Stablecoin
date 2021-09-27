2f14e091be1c3e6ad47edfae013a0608bec96986
JPeterD <71194096+JPeterD@users.noreply.github.com>
2021-09-26 23:02:18 -0400

    Added Oracle Support

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -2,24 +2,35 @@
 
 pragma solidity >=0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol";
+
 contract TimeLock {
 
     uint256 totalDeposits;
     uint256 numberset;
-    address public stableCoinAddress = 0xf9BfFeC0e8E58209eD9acfc75BbD961b152B935C;
+
+    address public stableCoinAddress = 0x4E39b8D4077508eAAb982d8E4DB1B7BF065b5016;
     LinoBToken public stableContract = LinoBToken(stableCoinAddress);
 
     mapping (address => uint256) public depositAmount;
     mapping (address => uint256) public stableCoinAmount;
 
+    FeedRegistryInterface internal ckb_usd_price_feed;
+
+    constructor() {
+        ckb_usd_price_feed = FeedRegistryInterface(0x1363bdCE312532F864e84924D54c7dA5eDB5B1BC);
+    }
+
     function deposit() public payable {
         require(msg.value > 0, "Deposit has to be greater than 0.");
         require(depositAmount[msg.sender] == 0, "You already have a deposit in the contract");
+        (uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound) = ckb_usd_price_feed.latestRoundData(0x0000000000000000000000000000000000000001, 0x0000000000000000000000000000000000000348);
 
-        depositAmount[msg.sender] = msg.value / 10e7;
-        totalDeposits = totalDeposits + msg.value / 10e7;
+
+        depositAmount[msg.sender] = msg.value;
+        totalDeposits = totalDeposits + msg.value;
         
-        uint256 stableMintAmount = msg.value / 10e7 / 2;
+        uint256 stableMintAmount = msg.value * uint256(price) / 10e7;
         stableContract.mint(msg.sender, stableMintAmount);
         stableCoinAmount[msg.sender] = stableMintAmount;
 
@@ -37,6 +48,16 @@ contract TimeLock {
 
         depositAmount[msg.sender] = 0;
         stableCoinAmount[msg.sender] = 0;
+    }
+
+    function getCKBUSDPrice() public view returns (int) {
+        (uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = ckb_usd_price_feed.latestRoundData(0x0000000000000000000000000000000000000001, 0x0000000000000000000000000000000000000348);
+        return price;
     }
 }
 
